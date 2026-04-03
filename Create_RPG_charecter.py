f6840fcf08194398fc1dea5f7abd7cd4c20e12e6
madhabendradasadhikaari <you@example.com>
2026-04-04 00:43:25 +0530

    Initial character creator code

--- Create_RPG_charecter.py
+++ Create_RPG_charecter.py
@@ -18,15 +18,51 @@ def create_character(character_name, STR, INT, CHA):
         return "The character should start with 7 points"
     return character_name + "\nSTR " +  full_dot * STR + empty_dot * (10 - STR) + "\nINT " + full_dot * INT + empty_dot * (10 - INT) + "\nCHA " + full_dot * CHA + empty_dot * (10 - CHA)
 
-character_name = str(input("Enter the character name:"))
-#add loop
-print("Each stats should be from 1 to 4 and total must be 7")
+#character creation
+while True:
+  character_name = input("Enter the character name:").strip()
 
+  if not character_name:
+    print("Name can not be empty.")
+    continue
+
+  if len(character_name) > 10:
+    print("The character name is too long (max 10 chars).")
+    continue
+  
+  break
+
+print(f"Success! The character name set to {character_name}")
+
+#stats
+print("Each stats should be from 1 to 4.")
+
+total_point = 7
+print(f"You have total {total_point} points.")
+
+#STR
 try:
     STR = int(input("Enter your character strength:"))
 except:
     print("You should only write number")
-INT = int(input("Enter your character intelligence:"))
-CHA = int(input("Enter your character charisma:"))
+  
+print(f"You have {total_point - STR} left!")
 
+#INT
+try:
+    INT = int(input("Enter your character intelligence:"))
+except:
+  print("You should only write number")
+
+print(f"You have {total_point - INT} left!")
+
+#CHA
+try:
+    CHA = int(input("Enter your character charisma:"))
+except:
+  print("You should only write number")
+
+print(f"You have {total_point - CHA} left!")
+
+#printing
 print(create_character(character_name, STR, INT, CHA))
